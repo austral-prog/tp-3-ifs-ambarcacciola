3dc8313d0cf63ec79d086b098d3a902f43aceb7b
ambarcacciola <acacciola@mail.austral.edu.ar>
2026-05-22 02:24:08 -0300

    Implement even-odd determination logic

--- exercise_even_odd.py
+++ exercise_even_odd.py
@@ -1,15 +1,6 @@
 def even_odd():
-    """
-    Ejercicio 2 - Par o Impar
-
-    Leer un número entero mediante input(). Determinar si el número es par o impar
-    e imprimir el resultado correspondiente.
-
-    Ejemplo:
-        Para la entrada "8", la salida esperada es:
-        El numero 8 es par
-
-        Para la entrada "7", la salida esperada es:
-        El numero 7 es impar
-    """
-    pass
+    num = int(input())
+    if num % 2 == 0:
+        print(f"El numero {num} es par")
+    else:
+        print(f"El numero {num} es impar")
